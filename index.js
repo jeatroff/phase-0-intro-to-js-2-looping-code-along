9ce011211fc857ab1c232942624c0d5c3f9e39f2
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2022-11-08 00:26:49 -0500

    commit tree 222db20964037fa6fa11ce5ab97c08268c4bd5fc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, adjective) {
+    var msgArray = [];
+    for(let i = 0; i < names.length; i++){
+        msgArray[i] = `Thank you, ${names[i]}, for the wonderful ${adjective} gift!`;
+    }
+    return msgArray;
+}
+
+function countDown(numCount) {
+    while(numCount >= 0) {
+        console.log(numCount);
+        numCount--;
+    }
+}
